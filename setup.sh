b3a0c301bfcf47561651c7f6837f25cde05a5e3e
Aldy Leon Garcia <aldyleongarcia@gmail.com>
2023-03-28 16:40:52 -0600

    venv

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-sudo apt update
+#sudo apt update
 sudo apt upgrade 
 sudo apt install mysql-server
-sudo service mysql start
-pip3 install pipenv
+#sudo service mysql restart
+sudo -H pip3 install pipenv
 pipenv install --skip-lock  -r ./requeriments
