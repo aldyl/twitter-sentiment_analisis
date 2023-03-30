0b8f2c2da5bef0851b66b478332bbaa686208322
Human <aldyleongarcia@gmail.com>
2023-03-30 17:02:26 +0000

    intento de graficar

--- main.py
+++ main.py
@@ -6,11 +6,10 @@ from tweets.tweets_handler import Tweets
 sn = SnscrapeTwiteer()
 tweet_handler = Tweets()
 
-tweets = sn.get_by_query("mexico cubaimport asyncio",
-                         2, since='2023-01-01', until='2023-03-29')
+tweets = sn.get_by_query("dias canel",
+                         1000, since='2023-01-01', until='2023-03-29')
 
 tweet_handler.tweet_to_json(tweets, 'util/file.json')
 
 print(tweet_handler.get_most_used_words(tweets, 10 ))
-
 
